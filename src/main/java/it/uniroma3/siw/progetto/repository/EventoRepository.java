bf124b8538610428b8eb616cd0ba922b18b37c1f
Luigi Romano <luigiromano417@gmail.com>
2026-06-11 18:23:59 +0200

    mi sa quasi finito UC7, manca controller

--- src/main/java/it/uniroma3/siw/progetto/repository/EventoRepository.java
+++ src/main/java/it/uniroma3/siw/progetto/repository/EventoRepository.java
@@ -1,8 +1,9 @@
 package it.uniroma3.siw.progetto.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.progetto.model.Evento;
 
-public interface EventoRepository extends CrudRepository<Evento, Long> {
+public interface EventoRepository extends JpaRepository<Evento, Long> {
 }
--- src/main/java/it/uniroma3/siw/progetto/repository/UtenteRepository.java
+++ src/main/java/it/uniroma3/siw/progetto/repository/UtenteRepository.java
@@ -1,8 +1,11 @@
 package it.uniroma3.siw.progetto.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.progetto.model.Utente;
 
 public interface UtenteRepository extends CrudRepository<Utente, Long> {
+    Optional<Utente> findByUsername(String username);
 }
